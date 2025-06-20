56cea2cfd0a52d9efb6bb1a8eb0bacb9a1703e9a
selim679 <salimfekih35@gmail.com>
2025-06-20 15:19:26 +0100

    first commit

---
+++ fonction.js
@@ -0,0 +1,29 @@
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("navbar").style.padding = "80px 10px";
+    document.getElementById("logo").style.fontSize = "35px";
+  }
+}
+
+
+
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    const faqs = document.querySelectorAll('.faq');
+
+    faqs.forEach(faq => {
+      faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+        const span = faq.querySelector('span');
+        span.textContent = faq.classList.contains('open') ? '-' : '+';
+      });
+    });
+  });
+
+
